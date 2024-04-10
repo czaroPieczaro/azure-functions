71f40d1fb91c3587ec8990c356ce137bd897ba86
Cezary Dudkiewicz <cezary.dudkiewicz@gft.com>
2024-04-10 14:43:47 +0200

    master

--- function_app.py
+++ function_app.py
@@ -7,19 +7,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    readings = req.params.get('readings')
+    print(readings)
+    for r in readings:
+        print(r)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(f"Hello, {readings}. This HTTP triggered function executed successfully.")
