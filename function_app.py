e5c084d2aa73e98ceb26c9378379205ad87531a4
Cezary Dudkiewicz <cezary.dudkiewicz@gft.com>
2024-04-10 15:21:54 +0200

    6

--- function_app.py
+++ function_app.py
@@ -10,8 +10,13 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 
     req_body = req.get_json()
     readings = req_body.get('readings')
-    # for r in readings:
-    #     r['status'] = 'ok'
+    for r in readings:
+        if r['temperature'] <= 25:
+            r['status'] = 'ok'
+        elif r['temperature'] < 50:
+            r['status'] = 'caution'
+        elif r['temperature'] < 50:
+            r['status'] = 'warning'
     response_dict = {"readings": readings}
     response_body = json.dumps(response_dict, indent=2)
     return func.HttpResponse(body=response_body, mimetype="application/json")
